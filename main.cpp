1e8897fbd00cb2903ac05b6f01b58da3702ee9b1
dukesook <devonsookhoo14@gmail.com>
2025-05-15 12:02:18 -0600

    read dng file

--- main.cpp
+++ main.cpp
@@ -3,8 +3,37 @@
 
 using namespace std;
 
-int main() {
-  LibRaw rawProcesor;
-  std::cout << "LibRaw version: " << LibRaw::version() << std::endl;
+int main(int argc, char *argv[]) {
+
+  const char *filename = "baseball.dng";
+  LibRaw rawProcessor;
+
+  int ret = rawProcessor.open_file(filename);
+  if (ret != LIBRAW_SUCCESS) {
+    cerr << "Failed to open file: " << libraw_strerror(ret) << endl;
+    return 1;
+  }
+
+  ret = rawProcessor.unpack();
+  if (ret != LIBRAW_SUCCESS) {
+    cerr << "Failed to unpack raw data: " << libraw_strerror(ret) << endl;
+    return 1;
+  }
+
+  cout << "Image dimensions: "
+       << rawProcessor.imgdata.sizes.width << " x "
+       << rawProcessor.imgdata.sizes.height << endl;
+
+  cout << "Color filter pattern: " << rawProcessor.imgdata.idata.cdesc << endl;
+
+  ushort *rawData = rawProcessor.imgdata.rawdata.raw_image;
+  if (!rawData) {
+    cerr << "raw_image is null (may not be Bayer format)." << endl;
+    return 1;
+  }
+
+  cout << "First pixel value: " << rawData[0] << endl;
+
+  rawProcessor.recycle();
   return 0;
 }
